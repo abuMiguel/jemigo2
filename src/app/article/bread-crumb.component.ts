c0c64dff5e569c8c76a1324816f25bfdd9693941
Jemigo <joseph.petersen2@gmail.com>
2025-09-18 20:51:56 -0400

    minor fixes

--- src/app/article/bread-crumb.component.ts
+++ src/app/article/bread-crumb.component.ts
@@ -5,7 +5,6 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'breadcrumb',
   standalone: true,
-  standalone: true,
   imports: [CommonModule, RouterLink],
   template: `
   <nav><p class="mt-0"><a [routerLink]="backRoute">{{backName}}</a></p></nav>
